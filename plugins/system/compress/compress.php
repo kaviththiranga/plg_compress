3a1a3944c669368c7a102cfcf00c45f32c8e4daa
kaviththiranga <rc404mekt@gmail.com>
2012-08-05 09:24:53 +0530

    new files added

--- plugins/system/compress/compress.php
+++ plugins/system/compress/compress.php
@@ -1,1 +1,33 @@
 <?php
+// no direct access
+    defined('_JEXEC') or die;
+
+/**
+ * Joomla! Assets Compress Plugin
+ *
+ * @package		Joomla.Plugin
+ * @subpackage	System.compress
+ */
+class plgSystemCompress extends JPlugin
+{
+    function __construct(& $subject, $config)
+    {
+        parent::__construct($subject, $config);
+
+        //Set the language in the class
+        $config = JFactory::getConfig();
+        $options = array(
+            'defaultgroup'	=> 'page',
+            'browsercache'	=> $this->params->get('browsercache', false),
+            'caching'		=> false,
+        );
+
+        $this->_cache = JCache::getInstance('page', $options);
+    }
+
+    function onBeforeRender()
+    {
+
+    }
+
+}
