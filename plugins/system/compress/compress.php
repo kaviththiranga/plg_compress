59a4cba820857d44efcadbc7a5cb94e442b6ca79
kaviththiranga <rc404mekt@gmail.com>
2012-08-13 08:57:00 +0530

    css methods implemented

--- plugins/system/compress/compress.php
+++ plugins/system/compress/compress.php
@@ -15,9 +15,11 @@ class plgSystemCompress extends JPlugin
 	public $scriptFiles;
 	public $scripts;
 	public $stylesheets;
-	var $styles;
-	var $compressedJsFiles ;
-	var $combinedJsFiles;
+	public $styles;
+	public $compressedJsFiles ;
+	public $combinedJsFiles;
+    public $compressedCssFiles ;
+    public $combinedCssFiles;
 
 	function __construct(& $subject, $config)
     {
@@ -40,11 +42,13 @@ class plgSystemCompress extends JPlugin
         $this->styles         = &$this->_document->_style;
         $this->compressedJsFiles = array();
         $this->combinedJsFiles   = array();
+        $this->compressedCssFiles = array();
+        $this->combinedCssFiles   = array();
     }
 
     function onBeforeCompileHead()
     {
-
+        //Avoid for the backend
         if(JFactory::getApplication()->isAdmin())
         {
             return;
@@ -55,58 +59,132 @@ class plgSystemCompress extends JPlugin
         // if only the compression is on, do the following, otherwise let the combiner take care of compression also
         if($this->_options['jscompression'] && !$this->_options['combinejs'])
         {
-           $compressionOptions = $this->_getCompressorOptions('js');
-
-           foreach($this->scriptFiles as $file => $attributes )
-           {
-               if(JMediaCompressor::compressFile(dirname(JPATH_SITE).$file, $compressionOptions))
-               {
-                   $destinationFile = str_ireplace('.js','.min.js', $file);
-                   $this->compressedJsFiles[$destinationFile] = $attributes;
-               }
-               else
-               {
-                   $this->compressedJsFiles[$file]= $attributes;
-               }
-           }
-           $this->scriptFiles = $this->compressedJsFiles;
+            $this->_compressJsFiles();
         }
 
         if ($this->_options['combinejs'])
         {
-            $currentFileSet = array();
-            $currentAttribs = array();
-            $fileCount      = 0;
-
-            foreach($this->scriptFiles as $file => $attributes)
-            {
-                if($fileCount === 0)
-                {
-                    $currentAttribs = $attributes;
-                    $currentFileSet[] = $file;
-                    $fileCount++;
-                    continue;
-                }
-                // Only combine files that have similar attributes, divide files into separate sets depending on attributes
-                if (md5(serialize($currentAttribs)) !== md5(serialize($attributes)))
-                {
-                    $combinedFile = $this->_combineJsFiles($currentFileSet);
-                    $this->combinedJsFiles[$combinedFile] = $currentAttribs;
-
-                    $currentAttribs = $attributes;
-                    $currentFileSet = array();
-                }
-                $fileCount++;
-                $currentFileSet[] = $file;
-
-                if(count($this->scriptFiles)===$fileCount)
-                {
-                    $combinedFile = $this->_combineJsFiles($currentFileSet);
-                    $this->combinedJsFiles[$combinedFile] = $currentAttribs;
-                 }
-            }
-            $this->scriptFiles = $this->combinedJsFiles;
+            $this->_prepareAndCombineJs();
         }
+        if($this->_options['csscompression'] && !$this->_options['combinecss'])
+        {
+            $this->_compressCssFiles();
+        }
+
+        if ($this->_options['combinecss'])
+        {
+            $this->_prepareAndCombineCss();
+        }
+    }
+
+    private function _compressJsFiles()
+    {
+        $compressionOptions = $this->_getCompressorOptions('js');
+
+        foreach($this->scriptFiles as $file => $attributes )
+        {
+            if(JMediaCompressor::compressFile(dirname(JPATH_SITE).$file, $compressionOptions))
+            {
+                $destinationFile = str_ireplace('.js','.min.js', $file);
+                $this->compressedJsFiles[$destinationFile] = $attributes;
+            }
+            else
+            {
+                $this->compressedJsFiles[$file]= $attributes;
+            }
+        }
+        $this->scriptFiles = $this->compressedJsFiles;
+    }
+
+    private function _compressCssFiles()
+    {
+        $compressionOptions = $this->_getCompressorOptions('css');
+
+        foreach($this->stylesheets as $file => $attributes )
+        {
+            if(JMediaCompressor::compressFile(dirname(JPATH_SITE).$file, $compressionOptions))
+            {
+                $destinationFile = str_ireplace('.css','.min.css', $file);
+                $this->compressedCssFiles[$destinationFile] = $attributes;
+            }
+            else
+            {
+                $this->compressedCssFiles[$file]= $attributes;
+            }
+        }
+        $this->scriptFiles = $this->compressedJsFiles;
+    }
+
+    private function _prepareAndCombineJs()
+    {
+        $currentFileSet = array();
+        $currentAttribs = array();
+        $fileCount      = 0;
+
+        foreach($this->scriptFiles as $file => $attributes)
+        {
+            if($fileCount === 0)
+            {
+                $currentAttribs = $attributes;
+                $currentFileSet[] = $file;
+                $fileCount++;
+                continue;
+            }
+            // Only combine files that have similar attributes, divide files into separate sets depending on attributes
+            if (md5(serialize($currentAttribs)) !== md5(serialize($attributes)))
+            {
+                $combinedFile = $this->_combineJsFiles($currentFileSet);
+                $this->combinedJsFiles[$combinedFile] = $currentAttribs;
+
+                $currentAttribs = $attributes;
+                $currentFileSet = array();
+            }
+            $fileCount++;
+            $currentFileSet[] = $file;
+
+            if(count($this->scriptFiles)===$fileCount)
+            {
+                $combinedFile = $this->_combineJsFiles($currentFileSet);
+                $this->combinedJsFiles[$combinedFile] = $currentAttribs;
+            }
+        }
+        $this->scriptFiles = $this->combinedJsFiles;
+    }
+
+    private function _prepareAndCombineCss()
+    {
+        $currentFileSet = array();
+        $currentAttribs = array();
+        $fileCount      = 0;
+
+        foreach($this->stylesheets as $file => $attributes)
+        {
+            if($fileCount === 0)
+            {
+                $currentAttribs = $attributes;
+                $currentFileSet[] = $file;
+                $fileCount++;
+                continue;
+            }
+            // Only combine files that have similar attributes, divide files into separate sets depending on attributes
+            if (md5(serialize($currentAttribs)) !== md5(serialize($attributes)))
+            {
+                $combinedFile = $this->_combineCssFiles($currentFileSet);
+                $this->combinedCssFiles[$combinedFile] = $currentAttribs;
+
+                $currentAttribs = $attributes;
+                $currentFileSet = array();
+            }
+            $fileCount++;
+            $currentFileSet[] = $file;
+
+            if(count($this->scriptFiles)===$fileCount)
+            {
+                $combinedFile = $this->_combineCssFiles($currentFileSet);
+                $this->combinedCssFiles[$combinedFile] = $currentAttribs;
+            }
+        }
+        $this->stylesheets = $this->combinedCssFiles;
     }
 
     private function _combineJsFiles($files)
@@ -125,6 +203,21 @@ class plgSystemCompress extends JPlugin
         return $destinationFile;
     }
 
+    private function _combineCssFiles($files)
+    {
+        $filesFullPath = array();
+        // Set full file path in order to combiner to work properly
+        foreach ($files as $file)
+        {
+            $filesFullPath[] = dirname(JPATH_SITE).$file;
+
+        }
+        $destinationFile = str_ireplace('.css','.combined.css', $files[0]);
+        JMediaCombiner::combineFiles($filesFullPath,$this->_getCombinerOptions('css'),dirname(JPATH_SITE).$destinationFile);
+
+        var_dump($this->_getCombinerOptions('css'));
+        return $destinationFile;
+    }
     private function _getCompressorOptions($type)
     {
         $tmp = explode(';', $this->params->get('compressoptions'));
